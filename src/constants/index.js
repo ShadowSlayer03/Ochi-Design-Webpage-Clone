2eba2bd437aba7103dae44cf84278eb3b331830b
ShadowSlayer03 <sslayer441@gmail.com>
2024-03-26 19:53:07 +0530

    Added constants required

---
+++ src/constants/index.js
@@ -0,0 +1,15 @@
+export const navTexts = ["Services","Our Work","About Us","Insights","Contact Us"];
+
+export const fydeText = ["Audit","Copywriting","Sales Deck","Slides Design"];
+
+export const viseText = ["Agency","Company Presentation"];
+
+export const trawaText = ["Brand Identity","Design Research", "Investor Deck"];
+
+export const premiumBlendText = ["Branded Template"];
+
+export const socials = ["Instagram","Facebook","Behance","LinkedIn"];
+
+export const location1 = ["202-1965 W 4th Ave","Vancouver, Canada"];
+
+export const location2 = ["30 Chukarina St","Lviv, Ukraine"];
